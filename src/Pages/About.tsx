5eaaa86fdd3594be2360625679c4e1603b6485d1
RANDRIANANTOANINA Mandresy Gaëtan <mandresy2020@gmail.com>
2026-03-02 22:15:25 +0300

    fix: resolve superposing boxes 3

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -8,7 +8,8 @@ function About() {
       id="about"
       className={
         `
-      w-full h-screen bg-gray-50 px-6 py-20 flex flex-col items-center gap-8 md:gap-12
+      w-full min-h-screen bg-gray-50 px-6 py-20 flex flex-col items-center gap-8 md:gap-12
+      border-4
       
         transition-all duration-1500 ease-out
         ${isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"}
