b66fc82935bd2d7232983c89f6254bcc8f7d7c4a
RANDRIANANTOANINA Mandresy Gaëtan <mandresy2020@gmail.com>
2026-03-05 22:06:11 +0300

    fix: bug related to toggle buton display

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -21,7 +21,7 @@ const ThemeToggle = () => {
       onClick={() => setDarkMode(!darkMode)}
       className="p-2 bg-white/30 backdrop-blur-xl border border-white/20
                  dark:bg-slate-900/40 dark:border-slate-700/50 dark:shadow-2xl fixed
-                 top-6 right-6
+                 top-6 right-6 z-9999
                  rounded-full shadow-lg"
     >
       {darkMode ? '🌙' : '☀️'}
