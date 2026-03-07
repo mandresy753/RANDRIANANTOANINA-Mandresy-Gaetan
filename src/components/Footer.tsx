8e53512cf6081acfcf57cbcfad63b8ffb22643ca
RANDRIANANTOANINA Mandresy Gaëtan <mandresy2020@gmail.com>
2026-03-07 20:03:43 +0300

    chore: add phone number in footer && make contact button to match with the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,25 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedin, faFacebook } from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
   return (
-    <footer className="w-full py-10 px-6 text-center pb-32
+    <footer id="footer" className="w-full py-10 px-6 text-center pb-32
       bg-gray-100 dark:bg-slate-900
       border-t border-gray-200 dark:border-slate-800">
           <p className="text-lg font-semibold mb-6 text-gray-700 dark:text-slate-300">
         Travaillons ensemble 🤝
       </p>
       <div className="flex justify-center gap-6 mb-6 text-2xl">
+        <a
+          target="_blank"
+          href="tel:+261344141732"
+          className="text-gray-600 hover:text-gray-900 dark:text-slate-400 dark:hover:text-gray-200 transition"
+          aria-label="Phone"
+        >
+          <FontAwesomeIcon icon={faPhone} />
+        </a>
+
         <a
           target="_blank"
           href="mailto:mandresy2020@gmail.com"
